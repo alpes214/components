13c82aec23dab0bbdf9d23a50318acd12ada96ba
Valiantsin Rakush <vrakush.external@epo.org>
2022-08-28 21:43:44 +0300

    cards

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
+import { faker } from "@faker-js/faker";
 import React from "react";
 import { createRoot } from 'react-dom/client';
 import ComponentDetail from './ComponentDetail.js';
+import { Faker } from "@faker-js/faker";
+import ApprovalCard from "./ApprovalCard.js";
 
 const App = () => {
     return (
-        <div>
-            <ComponentDetail />
-            <ComponentDetail />
-            <ComponentDetail />
-            <ComponentDetail />
+        <div className="ui container comments">
+            <ApprovalCard >
+                <ComponentDetail author="Sam" timeAgo="Todat at 4:45PM" text={faker.lorem.words()} avatar={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard >
+                <ComponentDetail author="Sam" timeAgo="Todat at 4:45PM" text={faker.lorem.words()} avatar={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard >
+                <ComponentDetail author="Sam" timeAgo="Todat at 4:45PM" text={faker.lorem.words()} avatar={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard >
+                <ComponentDetail author="Sam" timeAgo="Todat at 4:45PM" text={faker.lorem.words()} avatar={faker.image.avatar()} />
+            </ApprovalCard>
         </div>
     )
 }
